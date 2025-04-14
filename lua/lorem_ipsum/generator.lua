8ec8a29362017ee790ad9bab9f24be2a4e71ee4d
Daniel Roberto <sdanirobert@outlook.com>
2025-04-14 13:15:02 -0300

    style: add style config

--- lua/lorem_ipsum/generator.lua
+++ lua/lorem_ipsum/generator.lua
@@ -1,34 +1,35 @@
 local M = {}
 
 local lorem_ipsum_words = {
-	"lorem",
-	"ipsum",
-	"dolor",
-	"sit",
-	"amet",
-	"consectetur",
-	"adipiscing",
-	"elit",
-	"sed",
-	"do",
-	"eiusmod",
-	"tempor",
-	"incididunt",
-	"ut",
-	"labore",
-	"et",
-	"dolore",
-	"magna",
-	"aliqua",
+  "lorem",
+  "ipsum",
+  "dolor",
+  "sit",
+  "amet",
+  "consectetur",
+  "adipiscing",
+  "elit",
+  "sed",
+  "do",
+  "eiusmod",
+  "tempor",
+  "incididunt",
+  "ut",
+  "labore",
+  "et",
+  "dolore",
+  "magna",
+  "aliqua",
 }
 
 function M.generate(word_count)
-	local result = {}
-	for i = 1, word_count do
-		local random_index = math.random(1, #lorem_ipsum_words)
-		table.insert(result, lorem_ipsum_words[random_index])
-	end
-	return table.concat(result, " ")
+  local result = {}
+  for _ = 1, word_count do
+    local random_index = math.random(1, #lorem_ipsum_words)
+    table.insert(result, lorem_ipsum_words[random_index])
+  end
+  return table.concat(result, " ")
 end
 
 return M
+
--- plugin/lorem_ipsum.lua
+++ plugin/lorem_ipsum.lua
@@ -1,5 +1,6 @@
 local ok, cmp = pcall(require, "cmp")
 if ok then
-    local source = require("lorem_ipsum.source")
-    cmp.register_source("lorem_ipsum", source.new())
+  local source = require("lorem_ipsum.source")
+  cmp.register_source("lorem_ipsum", source.new())
 end
+
